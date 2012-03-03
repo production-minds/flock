06d2cc4d16bd781915e01bd22ffd9695df6f81f0
Dan Stocker <dan@kwaia.com>
2012-03-03 23:33:37 +0100

    constant delegation fixed

--- src/constants.js
+++ src/constants.js
@@ -3,12 +3,16 @@
  *
  * These constants have an effect on the traversal process.
  */
-var flock = flock || {};
+/*global flock */
+(function (utils) {
+    flock.constants = {
+        keys: 0,        // collect leaf keys
+        values: 1,      // collect leaf values
+        both: 2,        // collect key:value pairs of leaf nodes
+        del: 3,         // delete leaf nodes
+        count: 4        // count leaf nodes
+    };
 
-flock.constants = {
-    keys: 0,        // collect leaf keys
-    values: 1,      // collect leaf values
-    both: 2,        // collect key:value pairs of leaf nodes
-    del: 3,         // delete leaf nodes
-    count: 4        // count leaf nodes
-};
+    // delegating constants on flock object
+    utils.delegate(flock, flock.constants);
+}(flock.utils));
