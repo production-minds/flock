59cdbd9f3b47255727eb8b55fd87795767aa773f
Dan Stocker <dan@kwaia.com>
2012-03-04 13:20:25 +0100

    event methods take node instead of root & path

--- src/event.js
+++ src/event.js
@@ -6,19 +6,20 @@
 /*global flock */
 
 flock.event = (function (core, live) {
-    var self;
+    var ERROR_NONTRAVERSABLE = "Non-traversable datastore node.",
+        ERROR_HANDLERNOTFUNCTION = "Handler is not a function.",
+        self;
 
     self = {
         /**
          * Subscribes to datastore event.
-         * @param root {object} Datastore root.
-         * @param path {Array} Datastore path for subscription.
+         * @param node {object} Datastore node.
          * @param eventName {string} Name of event to subscribe to.
          * @param handler {function} Event handler.
          * @throws {string}
          */
-        subscribe: function (root, path, eventName, handler) {
-            var meta = core.get(root, path.concat([live.META])),
+        subscribe: function (node, eventName, handler) {
+            var meta = node[live.META],
                 handlers;
 
             if (typeof meta === 'object') {
@@ -35,75 +36,85 @@ flock.event = (function (core, live) {
                     // adding handler to event
                     handlers[eventName].push(handler);
                 } else {
-                    throw "flock.event.subscribe: Handler is not a function.";
+                    throw "flock.event.subscribe: " + ERROR_HANDLERNOTFUNCTION;
                 }
             } else {
-                throw "flock.event.subscribe: Invalid path or non-live datastore node.";
+                throw "flock.event.subscribe: " + ERROR_NONTRAVERSABLE;
             }
         },
 
         /**
          * Unsubscribes from datastore event.
-         * @param root {object} Datastore root.
-         * @param path {Array} Datastore path for subscription.
+         * @param node {object} Datastore node.
          * @param [eventName] {string} Name of event to subscribe to.
          * @param [handler] {function} Event handler.
+         * @throws {string} On untraversable node.
          */
-        unsubscribe: function (root, path, eventName, handler) {
-            var meta = core.get(root, path.concat([live.META])),
-                handlers = core.get(root, path.concat([live.META, 'handlers'])),
+        unsubscribe: function (node, eventName, handler) {
+            var meta = node[live.META],
+                handlers,
                 i;
 
-            if (typeof handlers === 'object') {
-                if (typeof handler === 'function') {
-                    // removing specified handler from among handlers
-                    if (typeof handlers === 'object' &&
-                        typeof eventName === 'string'
-                    ) {
-                        handlers = handlers[eventName];
-                        for (i = 0; i < handlers.length; i++) {
-                            if (handlers[i] === handler) {
-                                handlers.splice(i, 1);
-                                break;
+            if (typeof meta === 'object') {
+                handlers = meta.handlers;
+                if (typeof handlers === 'object') {
+                    if (typeof handler === 'function') {
+                        // removing specified handler from among handlers
+                        if (typeof handlers === 'object' &&
+                            typeof eventName === 'string'
+                            ) {
+                            handlers = handlers[eventName];
+                            for (i = 0; i < handlers.length; i++) {
+                                if (handlers[i] === handler) {
+                                    handlers.splice(i, 1);
+                                    break;
+                                }
                             }
                         }
+                    } else if (typeof eventName === 'string') {
+                        // removing all handlers for a specific event
+                        if (typeof handlers === 'object' &&
+                            handlers.hasOwnProperty(eventName)
+                            ) {
+                            delete handlers[eventName];
+                        }
+                    } else {
+                        // removing all handlers altogether
+                        delete meta.handlers;
                     }
-                } else if (typeof eventName === 'string') {
-                    // removing all handlers for a specific event
-                    if (typeof handlers === 'object' &&
-                        handlers.hasOwnProperty(eventName)
-                    ) {
-                        delete handlers[eventName];
-                    }
-                } else {
-                    // removing all handlers altogether
-                    delete meta.handlers;
                 }
+            } else {
+                throw "flock.event.unsubscribe: " + ERROR_NONTRAVERSABLE;
             }
         },
 
         /**
          * Triggers event on specified datastore path.
-         * @param root {object} Datastore root.
-         * @param path {Array} Datastore path for subscription.
+         * @param node {object} Datastore node.
          * @param eventName {string} Name of event to subscribe to.
          * @param data {object} Custom data to be passed to event handlers.
+         * @throws {string} On untraversable node.
          */
-        trigger: function (root, path, eventName, data) {
-            var handlers = core.get(root, path.concat([live.META, 'handlers', eventName])),
+        trigger: function (node, eventName, data) {
+            var meta = node[live.META],
+                handlers,
                 i;
 
-            if (typeof handlers === 'object') {
-                // calling handlers for event
-                for (i = 0; i < handlers.length; i++) {
-                    handlers[i](root, path, data);
-                }
+            if (typeof meta === 'object') {
+                handlers = (meta.handlers || {})[eventName];
+                if (typeof handlers === 'object') {
+                    // calling handlers for event
+                    for (i = 0; i < handlers.length; i++) {
+                        handlers[i](node, data);
+                    }
 
-                // bubbling event up the datastore tree
-                if (path.length > 1) {
-                    path.pop();
-                    self.trigger(root, path, eventName, data);
+                    // bubbling event up the datastore tree
+                    if (typeof meta.parent === 'object') {
+                        self.trigger(meta.parent, eventName, data);
+                    }
                 }
+            } else {
+                throw "flock.event.trigger: " + ERROR_NONTRAVERSABLE;
             }
         }
     };
--- src/event.test.js
+++ src/event.test.js
@@ -18,24 +18,24 @@
         function testHandler() { }
 
         raises(function () {
-            event.subscribe(data, ['hello', 'world'], 'testEvent', "nonFunction");
+            event.subscribe(data.hello.world, 'testEvent', "nonFunction");
         }, "Invalid event handler raises error.");
         raises(function () {
-            event.subscribe(data, ['hello', 'world', 'center'], 'testEvent', testHandler);
+            event.subscribe(data.hello.world.center, 'testEvent', testHandler);
         }, "Invalid datastore node raises error.");
 
-        event.subscribe(data, ['hello', 'world'], 'testEvent', testHandler);
-        event.subscribe(data, ['hello'], 'otherEvent', testHandler);
+        event.subscribe(data.hello.world, 'testEvent', testHandler);
+        event.subscribe(data.hello, 'otherEvent', testHandler);
         equal(data.hello.world[live.META].handlers.testEvent[0], testHandler, "Event handler added");
         equal(data.hello[live.META].handlers.otherEvent[0], testHandler, "Other event handler added");
 
-        event.unsubscribe(data, ['hello', 'world'], 'testEvent', testHandler);
+        event.unsubscribe(data.hello.world, 'testEvent', testHandler);
         equal(data.hello.world[live.META].handlers.testEvent.length, 0, "Event handler removed");
 
-        event.unsubscribe(data, ['hello', 'world'], 'testEvent');
+        event.unsubscribe(data.hello.world, 'testEvent');
         equal(data.hello.world[live.META].handlers.hasOwnProperty('testEvent'), false, "Event handlers removed for given event");
 
-        event.unsubscribe(data, ['hello', 'world']);
+        event.unsubscribe(data.hello.world);
         equal(data.hello.world[live.META].hasOwnProperty('handlers'), false, "All event handlers removed from node");
     });
 
@@ -44,22 +44,22 @@
         function testHandler() { i++; }
         function otherHandler() { j++; }
 
-        event.subscribe(data, ['hello', 'world'], 'testEvent', testHandler);
-        event.subscribe(data, ['hello'], 'testEvent', otherHandler);
-        event.subscribe(data, ['hello', 'world'], 'otherEvent', otherHandler);
+        event.subscribe(data.hello.world, 'testEvent', testHandler);
+        event.subscribe(data.hello, 'testEvent', otherHandler);
+        event.subscribe(data.hello.world, 'otherEvent', otherHandler);
 
         i = j = 0;
-        event.trigger(data, ['hello', 'world'], 'otherEvent');
+        event.trigger(data.hello.world, 'otherEvent');
         equal(j, 1, "Event triggered");
 
         i = j = 0;
-        event.trigger(data, ['hello', 'world'], 'testEvent');
+        event.trigger(data.hello.world, 'testEvent');
         equal(i, 1, "Event triggered on source node");
         equal(j, 1, "Event bubbled to parent");
 
-        i = j = 0;
-        event.trigger(data, ['hello', 'all'], 'testEvent');
-        ok((i === 0) && (j === 0), "Triggering event on unsubscribed path doesn't trigger event");
+        raises(function () {
+            event.trigger(data.hello.all, 'testEvent');
+        }, "Triggering event on ordinal node raises error");
     });
 }(flock.live,
     flock.event));
