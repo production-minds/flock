9a17b94266698559e3973d6285aace7d81612491
Dan Stocker <dan@kwaia.com>
2012-03-06 10:07:51 +0100

    query internal functions factored out to static scope

--- src/query.js
+++ src/query.js
@@ -8,10 +8,154 @@ flock.query = (function (constants, utils) {
         RE_PATH_SKIPPER = /\.{2,}/,
 
         ignoredKey,
-        errors, self;
+        errors, privates, self;
 
     errors = {
         ERROR_INVALIDPATH: "Invalid path."
+    };
+
+    privates = {
+        /**
+         * Processes key node.
+         * TODO: getting rid of i, obj, and depth params
+         * @param key {string} Key in object to proceed to.
+         * @param obj {object} Node to walk.
+         * @param i {number} Current position in path,
+         * @param depth {number} Current depth in tree.
+         * @return {boolean} Whether to terminate traversal.
+         * @this {object} Traversal state object.
+         */
+         node: function (key, i, obj, depth) {
+            if (key === ignoredKey) {
+                return false;
+            }
+
+            var value,
+                that = this;
+
+            if (i < that.last) {
+                // current node has children, burrowing one level deeper
+                if (obj.hasOwnProperty(key)) {
+                    privates.walk.call(that, obj[key], i + 1, depth + 1);
+                }
+            } else {
+                // leaf node reached
+                if (typeof that.options.mode !== 'undefined') {
+                    // when querying or deleting
+                    value = obj[key];
+                    if (that.options.undef || typeof value !== 'undefined') {
+                        switch (that.options.mode) {
+                        case constants.VALUES:
+                            // collecting value from nodes
+                            that.result.push(value);
+                            break;
+                        case constants.KEYS:
+                            // collecting key from node
+                            that.result.push(key);
+                            break;
+                        case constants.BOTH:
+                            // collecting key AND value from node
+                            // WARNING: new values with same key overwrite old
+                            that.result[key] = value;
+                            break;
+                        case constants.DEL:
+                            // deleting node
+                            delete obj[key];
+                            break;
+                        case constants.COUNT:
+                            // counting node
+                            that.result++;
+                            break;
+                        }
+                        if (--that.limit === 0) {
+                            return true;
+                        }
+                    }
+                } else {
+                    // when updating
+                    if (typeof that.options.value === 'function') {
+                        // calling custom handler on node
+                        value = that.options.value(obj[key]);
+                        if (typeof value !== 'undefined') {
+                            obj[key] = value;
+                        }
+                    } else {
+                        // assigning custom value to key
+                        obj[key] = that.options.value;
+                    }
+                    if (--that.limit === 0) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        },
+
+        /**
+         * Collects end nodes. Used internally.
+         * @param obj {object} Node to walk.
+         * @param i {number} Current position in path,
+         * @param depth {number} Current depth in tree.
+         * @this {object} Traversal state object.
+         */
+        walk: function walk(obj, i, depth) {
+            var key, j,
+                that = this;
+
+            // detecting loopback
+            if (!that.loopback) {
+                for (j = 0; j < depth; j++) {
+                    if (obj === that.stack[j]) {
+                        return;
+                    }
+                }
+                // putting current object on the stack
+                that.stack[depth] = obj;
+            }
+
+
+            // processing next key in path
+            key = that.path[i];
+            if (key === '*') {
+                // processing wildcard node
+                for (key in obj) {
+                    if (obj.hasOwnProperty(key)) {
+                        if (privates.node.call(that, key, i, obj, depth)) {
+                            return;
+                        }
+                    }
+                }
+            } else if (key === null && typeof obj === 'object') {
+                // processing skipper node
+                // must be object type as strings have indexes, too
+                for (key in obj) {
+                    if (obj.hasOwnProperty(key)) {
+                        if (key === that.path[i + 1]) {
+                            // current key matches next key in path
+                            // re-walking current object but leving skipper key
+                            privates.walk.call(that, obj, i + 1, depth);
+                        } else {
+                            // current key doesn't match next key in path
+                            // walking next level, but staying on skipper key
+                            privates.walk.call(that, obj[key], i, depth + 1);
+                        }
+                    }
+                }
+            } else if (key instanceof Array) {
+                // processing list of nodes
+                for (j = 0; j < key.length; j++) {
+                    if (privates.node.call(that, key[j], i, obj, depth)) {
+                        return;
+                    }
+                }
+            } else {
+                // processing single node
+                key = that.path[i];
+                if (privates.node.call(that, key, i, obj, depth)) {
+                    return;
+                }
+            }
+        }
     };
 
     self = {
@@ -81,12 +225,12 @@ flock.query = (function (constants, utils) {
          * @param node {object} Datastore root.
          * @param path {string|Array} Datastore path expression.
          * @param [options] {object} Options.
-         *     - limit: max number of entries to retrieve, default: unlimited
-         *     - mode: type of return value is Object or Array (flock.key/flock.values/flock.both/flock.del), default: flock.array
-         *     - loopback: whether to traverse loopbacks, default: false
-         *     - undef: whether to collect undefined entries, default: false
-         *     - value: value to set, or callback function to execute on nodes
-         *       when undefined, function returns collected values
+         *  - limit: max number of entries to retrieve, default: unlimited
+         *  - mode: type of return value is Object or Array (flock.key/flock.values/flock.both/flock.del), default: flock.array
+         *  - loopback: whether to traverse loopbacks, default: false
+         *  - undef: whether to collect undefined entries, default: false
+         *  - value: value to set, or callback function to execute on nodes
+         *    when undefined, function returns collected values
          * @return {object} Collected nodes.
          */
         query: function (node, path, options) {
@@ -100,152 +244,27 @@ flock.query = (function (constants, utils) {
             }
 
             var tpath = typeof path === 'object' ? path.concat([]) : self.normalizePath(path),
-                last = tpath.length - 1,
-                limit = options.limit || 0,
-                loopback = options.loopback || false,
-                result = {2: {}, 4: 0}[options.mode] || [],
-                stack = options.loopback ? null : [];
+                state;
 
             // default case
             if (!tpath.length) {
                 return node;
             }
 
-            /**
-             * Collects end nodes.
-             * @param obj {object} Node to walk.
-             * @param i {number} Current position in path,
-             * @param depth {number} Current depth in tree.
-             * TODO: must be class level, creating a function on each call
-             * to .many() may impact performance significantly
-             */
-            (function walk(obj, i, depth) {
-                var key, j;
+            state = {
+                options: options,
+                path: tpath,
+                last: tpath.length - 1,
+                limit: options.limit || 0,
+                loopback: options.loopback || false,
+                result : {2: {}, 4: 0}[options.mode] || [],
+                stack: options.loopback ? null : []
+            };
 
-                // detecting loopback
-                if (!loopback) {
-                    for (j = 0; j < depth; j++) {
-                        if (obj === stack[j]) {
-                            return;
-                        }
-                    }
-                    // putting current object on the stack
-                    stack[depth] = obj;
-                }
+            // walking sub-tree and collecting matching nodes
+            privates.walk.call(state, node, 0, 0);
 
-                /**
-                 * Processes key node.
-                 * @param key {string} Key in object to proceed to.
-                 * @return {boolean} Whether to terminate traversal.
-                 */
-                function node(key) {
-                    if (key === ignoredKey) {
-                        return false;
-                    }
-
-                    var value;
-                    if (i < last) {
-                        // current node has children, burrowing one level deeper
-                        if (obj.hasOwnProperty(key)) {
-                            walk(obj[key], i + 1, depth + 1);
-                        }
-                    } else {
-                        // leaf node reached
-                        if (typeof options.mode !== 'undefined') {
-                            // when querying or deleting
-                            value = obj[key];
-                            if (options.undef || typeof value !== 'undefined') {
-                                switch (options.mode) {
-                                case constants.VALUES:
-                                    // collecting value from nodes
-                                    result.push(value);
-                                    break;
-                                case constants.KEYS:
-                                    // collecting key from node
-                                    result.push(key);
-                                    break;
-                                case constants.BOTH:
-                                    // collecting key AND value from node
-                                    // WARNING: new values with same key overwrite old
-                                    result[key] = value;
-                                    break;
-                                case constants.DEL:
-                                    // deleting node
-                                    delete obj[key];
-                                    break;
-                                case constants.COUNT:
-                                    // counting node
-                                    result++;
-                                    break;
-                                }
-                                if (--limit === 0) {
-                                    return true;
-                                }
-                            }
-                        } else {
-                            // when updating
-                            if (typeof options.value === 'function') {
-                                // calling custom handler on node
-                                value = options.value(obj[key]);
-                                if (typeof value !== 'undefined') {
-                                    obj[key] = value;
-                                }
-                            } else {
-                                // assigning custom value to key
-                                obj[key] = options.value;
-                            }
-                            if (--limit === 0) {
-                                return true;
-                            }
-                        }
-                    }
-                    return false;
-                }
-
-                // processing next key in path
-                key = tpath[i];
-                if (key === '*') {
-                    // processing wildcard node
-                    for (key in obj) {
-                        if (obj.hasOwnProperty(key)) {
-                            if (node(key)) {
-                                return;
-                            }
-                        }
-                    }
-                } else if (key === null && typeof obj === 'object') {
-                    // processing skipper node
-                    // must be object type as strings have indexes, too
-                    for (key in obj) {
-                        if (obj.hasOwnProperty(key)) {
-                            if (key === tpath[i + 1]) {
-                                // current key matches next key in path
-                                // re-walking current object but leving skipper key
-                                walk(obj, i + 1, depth);
-                            } else {
-                                // current key doesn't match next key in path
-                                // walking next level, but staying on skipper key
-                                walk(obj[key], i, depth + 1);
-                            }
-                        }
-                    }
-                } else if (key instanceof Array) {
-                    // processing list of nodes
-                    for (j = 0; j < key.length; j++) {
-                        if (node(key[j])) {
-                            return;
-                        }
-                    }
-                } else {
-                    // processing single node
-                    key = tpath[i];
-                    if (node(key)) {
-                        return;
-                    }
-                }
-            }(node, 0, 0));
-
-            return result;
+            return state.result;
         }
     };
 
